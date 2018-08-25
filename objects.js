cf1f1bc7b01aa53bb4ea5204eb223d46a7630230
Cedric David <cedric.david@btopenworld.com>
2018-08-25 12:25:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {'der': 'fre'};
 
-function updatePlayList(playlist, artist, song) {
+function updatePlaylist(playlist, artist, song) {
   playlist[artist] = song;
   return playlist;
 }
